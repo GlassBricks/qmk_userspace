002be7aec092fc748f936cb148c584e4e29975cf
GlassBricks <24237065+GlassBricks@users.noreply.github.com>
2025-01-26 19:06:15 -0600

    Swap repeat and num keys

--- keyboards/splitkb/halcyon/kyria/keymaps/glassbricks/keymap.c
+++ keyboards/splitkb/halcyon/kyria/keymaps/glassbricks/keymap.c
@@ -35,8 +35,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_BASE] = LAYOUT_split_3x6_5_hlc(
 		KC_TAB,		KC_Q,		KC_W,		KC_F,		KC_P,		KC_B,															    KC_J,		KC_L,		KC_U,		KC_Y,		KC_SCLN,	KC_MUTE,
 		KC_ESC,		KC_A,		KC_R,		KC_S,		KC_T,		KC_G,														   		KC_M,		KC_N,		KC_E,		KC_I,		KC_O,		KC_MINS,
-		KC_LCTL,	KC_Z,		KC_X,		KC_C,		KC_D,		KC_V,		KC_SPC, 	NUM_BLS,			MO(_EXT),	KC_UNDS,	KC_K,		KC_H,		KC_COMM,	KC_DOT,		KC_QUOT,	KC_ENT,
-											KC_LGUI,	XXXXXXX,    KC_LSFT,	MO(_EXT),	QK_REP,			    FUN_NUM,	KC_SPC,		MO(_SYM),	KC_LALT,	MO(_ADJ),
+		KC_LCTL,	KC_Z,		KC_X,		KC_C,		KC_D,		KC_V,		KC_SPC, 	QK_REP,			MO(_EXT),	KC_UNDS,	KC_K,		KC_H,		KC_COMM,	KC_DOT,		KC_QUOT,	KC_ENT,
+											KC_LGUI,	XXXXXXX,    KC_LSFT,	MO(_EXT),	NUM_BLS,			    FUN_NUM,	KC_SPC,		MO(_SYM),	KC_LALT,	MO(_ADJ),
                                             KC_MUTE,    KC_NO,      KC_NO,      KC_NO,      KC_NO,              KC_MUTE,     KC_NO,     KC_NO,      KC_NO,      KC_NO
 	),
 	[_SYM] = LAYOUT_split_3x6_5_hlc(
